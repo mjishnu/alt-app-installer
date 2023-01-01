d7abf907d86f6c9f6012e77dad487ea8bbe92722
m-jishnu <jishnum499@gmail.com>
2023-01-01 20:45:22 +0530

    add some docs

--- url_gen.py
+++ url_gen.py
@@ -24,7 +24,7 @@ def url_generator(url):
         raise Exception(
             'No Data Found: --> [You Selected Wrong Page in App Selector, Try Again!]')
 
-    #getting cat id and other details
+    #getting cat_id and package name from the api
     details_api = "https://displaycatalog.mp.microsoft.com/v7.0/products/"
 
     data = {"bigIds": product_id, "market": "US", "languages": "en"}
@@ -38,11 +38,11 @@ def url_generator(url):
     match1 = pattern1.search(str(data_list))
     match2 = pattern2.search(str(data_list))
 
-    cat_id = match1.group(1) #wucategoryid
+    cat_id = match1.group(1) 
     file_name = match2.group(1).split('_')[0]
-
-    #trying to get download links
     release_type = "Retail"
+
+    #getting the encrypted cookie for the fe3 delivery api
     with open("./data/xml/GetCookie.xml", "r") as f:
         cookie_content = f.read()
 
@@ -52,12 +52,15 @@ def url_generator(url):
         headers={'Content-Type': 'application/soap+xml; charset=utf-8'},
         verify=False
     )
-    doc1 = minidom.parseString(out.text)
-    cookie = doc1.getElementsByTagName('EncryptedData')[0].firstChild.nodeValue
+    doc = minidom.parseString(out.text)
 
+    #extracting the cooking from the EncryptedData tag
+    cookie = doc.getElementsByTagName('EncryptedData')[0].firstChild.nodeValue
+
+    #getting the update id,revision number and package name from the fe3 delivery api by providing the encrpyted cookie, cat_id, realse type
+    # Map {"retail": "Retail", "release preview": "RP","insider slow": "WIS", "insider fast": "WIF"}
     with open("./data/xml/WUIDRequest.xml", "r") as f:
         cat_id_content = f.read().format(cookie, cat_id, release_type)
-        
 
     out = session.post(
         'https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx',
@@ -66,48 +69,59 @@ def url_generator(url):
         verify=False
     )
     
-    doc1 = minidom.parseString(html.unescape(out.text))
-    filenames = {}
-    for node in doc1.getElementsByTagName('Files'):
+    doc = minidom.parseString(html.unescape(out.text))
+    filenames = {} #{ID: filename}
+    #extracting all the filenames(package name) from the xml (the file names are found inside the blockmap)
+    for node in doc.getElementsByTagName('Files'):
         filenames[node.parentNode.parentNode.getElementsByTagName(
             'ID')[0].firstChild.nodeValue] = f"{node.firstChild.attributes['InstallerSpecificIdentifier'].value}_{node.firstChild.attributes['FileName'].value}"
         pass
-
-    identities = []
-    for node in doc1.getElementsByTagName('SecuredFragment'):
+    
+    #extracting the update id,revision number from the xml
+    identities = {} #{filename: (update_id, revision_number)}
+    for node in doc.getElementsByTagName('SecuredFragment'):
         filename = filenames[node.parentNode.parentNode.parentNode.getElementsByTagName('ID')[
             0].firstChild.nodeValue]
-        update_identity = node.parentNode.parentNode.firstChild
-        identities += [(update_identity.attributes['UpdateID'].value,
-                        update_identity.attributes['RevisionNumber'].value, filename)]
 
+        update_identity = node.parentNode.parentNode.firstChild
+        identities[filename] = (update_identity.attributes['UpdateID'].value,
+                                update_identity.attributes['RevisionNumber'].value)
+                                
+    #parsing the filenames according to latest version,favorable types,system arch
+    #have to implement later
+
+
+    #getting the download url for the files
     with open("./data/xml/FE3FileUrl.xml", "r") as f:
         file_content = f.read()
-
-
-    def geturl(i, v, f):
+    
+    file_dict = {} #the final result
+    def geturl(updateid, revisionnumber, file_name):
         out = session.post(
             'https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx/secured',
-            data=file_content.format(i, v, release_type),
+            data=file_content.format(updateid, revisionnumber, release_type),
             headers={'Content-Type': 'application/soap+xml; charset=utf-8'},
             verify=False
         )
-        doc2 = minidom.parseString(out.text)
-        #checks for all the tags which have name filelocation if 2 of em have then it will check both
-        for i in doc2.getElementsByTagName("FileLocation"):
+        doc = minidom.parseString(out.text)
+        #checks for all the tags which have name "filelocation" and extracts the url from it
+        for i in doc.getElementsByTagName("FileLocation"):
             url = i.getElementsByTagName("Url")[0].firstChild.nodeValue
-            #to avoid getting block maps
+            #here there are 2 filelocation tags one for the blockmap and one for the actual file so we are checking for the length of the url
             if (len(url) != 99):
-                file_dict[f] = url
+                file_dict[file_name] = url
 
+    #using threading to concurrently get the download url for all the files 
     threads = []
-    file_dict = {}
-    for i, v, f in identities:
-        th = Thread(target=geturl, args=(i, v, f))
+    for key,value in identities:
+        file_name = key
+        updateid,revisionnumber = value
+        th = Thread(target=geturl, args=(updateid, revisionnumber, file_name))
         th.daemon = True
         threads.append(th)
         th.start()
 
+    #waiting for all threads to complete
     for th in threads:
         th.join()
 
