e177e04c3233829108d9d5e4a140e74738f847e9
m-jishnu <jishnum499@gmail.com>
2023-01-25 18:17:37 +0530

    typo fix

--- app/utls.py
+++ app/utls.py
@@ -43,7 +43,7 @@ def install(path):
     else:
         msg = 'Failed To Install Dependencies!'
         detail_msg = 'In some cases, this occurs since the dependencies are already installed on your pc. '
-        detail_msg += 'So check wheather the program is installed in start menu.\n\n'
+        detail_msg += 'So check wheather the program is installed from start menu.\n\n'
         detail_msg += 'if the app is not installed, Enable [Dependencies --> Ignore Version], '
         detail_msg += 'If the problem still exists Enable [Dependencies --> Ignore All Filters]'
         endresult = (msg, detail_msg, "Warning")
